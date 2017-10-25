6490ed075e0990cb368e4fe26a77a56a3ad74ce2
Filip Kubat <filip.kubat@wp.pl>
2017-10-25 19:12:47 +0200

    Add login and register views

---
+++ OnlineShop/OnlineShop/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using OnlineShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel loginViewModel, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+                return View(loginViewModel);
+            else
+                return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(registerViewModel);
+            else
+                return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ OnlineShop/OnlineShop/ViewModels/AccountViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage="You have to enter e-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "You have to enter password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "You have to enter e-mail")]
+        [EmailAddress(ErrorMessage = "E-mail is wrong")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "You have to enter password")]
+        [StringLength(30, ErrorMessage = "{0} have to include at least {2} characters.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = " Password ")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = " Confirm Password ")]
+        [Compare("Password", ErrorMessage ="Passwords are not the same.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
